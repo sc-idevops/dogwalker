9b77c91945508a4095eb4ea1b5713d53e39b2a92
sc-idevops <steveokard@gmail.com>
2023-10-04 23:35:55 -0400

    refinement on ffmpeg parameters for streaming

--- RTMPS.py
+++ RTMPS.py
@@ -10,7 +10,8 @@ while True:
     files = os.listdir(dirname)
     for f in files:
         if ".mp4" in f:
-            cmd = "ffmpeg -threads 3 -re -i " + '"' + dirname + f + '"' + " -bufsize 6000k -maxrate 4500k -b:v " + brate + \
+            cmd = "ffmpeg -re -i " + '"' + dirname + f + '"' + \
+                " -bufsize 6000k -maxrate 4500k -b:v " + brate + \
                 " -c:v libx264 -movflags +faststart -preset ultrafast -crf 24 -g 60 -f flv " + key
             print(cmd)
             os.system(cmd)
